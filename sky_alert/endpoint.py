1e6022bad74d847ae6126170e47c868dd6d6a8b9
Rohan Sahgal <rohansahgal@hotmail.com>
2023-12-18 15:34:03 -0500

    Added requests package to Poetry and grabbed sunrise/sunset data from OpenWeather

--- sky_alert/endpoint.py
+++ sky_alert/endpoint.py
@@ -1,4 +1,10 @@
 from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
+import requests
+import datetime
+
+load_dotenv()
 
 app = FastAPI()
 
@@ -8,5 +14,21 @@ def healthz():
     return 'OK'
 
 @app.get('/sunrise')
-def sunrise():
-    return '06:00'
+def sunrise(lat=0, lon=0):
+
+    headers = {
+        'Content-Type': 'application/json',
+    }
+
+    params = {
+        "lat": lat,
+        "lon": lon,
+        "appid": os.environ["OPENWEATHER_API_KEY"]
+    }
+    
+    res = requests.get(os.environ["OPENWEATHER_API_URL"], params=params, headers=headers)
+
+    sunrise = datetime.datetime.utcfromtimestamp(res.json()['current']['sunrise'])
+    sunset = datetime.datetime.utcfromtimestamp(res.json()['current']['sunset'])
+
+    return (sunrise, sunset)
